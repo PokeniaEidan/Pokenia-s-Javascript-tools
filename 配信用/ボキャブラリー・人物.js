5ee1944126e8c09f1b6c3caba4eb027fba847e0d
PokeniaEidan <pokenia.eidan@gmail.com>
2026-05-04 22:36:42 +0900

    Add files via upload

--- 配信用/ボキャブラリー・人物.js
+++ 配信用/ボキャブラリー・人物.js
@@ -16,13 +16,11 @@ function 人物関数(){
 `小野かのこ`,
 ]
 
-
-
 人物乱数=Math.floor(Math.random() * 人物リスト.length);
 
 人物=`<font color="khaki ">${人物リスト[人物乱数]}</font>`
 
-
+小野かのこ=`<font color="khaki ">小野かのこ</font>`
 
 男性リスト=[
 `石破茂`,
